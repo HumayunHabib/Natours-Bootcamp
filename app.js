ddbbe20a15babc8f74bb6f6bb6a5e1318d466584
HumayunHabib <humayun.habib@magnusmage.com>
2022-07-24 12:22:26 +0500

    tours api

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
+const fs = require('fs');
 const express = require('express');
-
 const app = express();
+app.use(express.json());
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+// app.post('/', (req, res) => {
+//   res.send('You can post to this end point...');
+// });
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'status',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
-app.post('/', (req, res) => {
-  res.send('You can post to this end point...');
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'sucess',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 const port = 3000;
 app.listen(port, () => {
