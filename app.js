d9b73e276e49613b47b10079c6b05e509c4783ca
HumayunHabib <humayun.habib@magnusmage.com>
2022-07-24 14:19:16 +0500

    delete  tour

--- app.js
+++ app.js
@@ -78,6 +78,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     },
   });
 });
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: {
+      tour: null,
+    },
+  });
+});
 const port = 3000;
 app.listen(port, () => {
   console.log(`app running on port ${port}...`);
