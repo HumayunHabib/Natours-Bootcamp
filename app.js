3e890f1950ff9a544e704c3816f7af0108e46f76
HumayunHabib <humayun.habib@magnusmage.com>
2022-07-24 13:09:11 +0500

    find single tour

--- app.js
+++ app.js
@@ -16,13 +16,33 @@ const tours = JSON.parse(
 );
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    status: 'status',
+    status: 'success',
     results: tours.length,
     data: {
       tours,
     },
   });
 });
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
